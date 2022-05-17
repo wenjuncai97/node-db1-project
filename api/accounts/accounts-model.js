47981e435a13ccf2540768cf189cb65a13503c90
Nathan Cai <wenjuncai97@gmail.com>
2022-05-16 20:14:48 -0400

    finished all tests

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,23 +1,30 @@
+const res = require('express/lib/response');
 const db = require('../../data/db-config');
 
 const getAll = () => {
-  return db('accounts').first();
+  // select * from accounts;
+  return db('accounts');
 }
 
 const getById = id => {
-  return db('accounts').where({accountid: id}).first();
+  // select * from accounts where id = 1;
+  return db('accounts').where({id: id}).first();
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  // insert into accounts (name, budget) values ('foo', '1000');
+  const [id] = await db('accounts').insert(account);
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  await db('accounts').where('id', id).update(account)
+  return getById(id)
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  // delete from accounts where id = 1;
+  return db('accounts').where('id', id).delete()
 }
 
 module.exports = {
